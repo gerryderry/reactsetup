5b283e9bb2d47a767123ed6920400761a124b0a9
George McLaughlin <gerryderry@hotmail.com>
2016-10-30 14:58:44 +0000

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import AwesomeComponent from './AwesomeComponent.js';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <p> Hello React!!!!!</p>
+        <AwesomeComponent />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack')
+var path = require('path')
+
+var BUILD_DIR = path.resolve(__dirname + '/app/build')
+var APP_DIR = path.resolve(__dirname + '/app')
+
+var config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    publicPth: '/'
+  },
+  //for dev env only
+  // devtool: 'source-map',
+  // devServer: {
+  //   inline: true,
+  //   contentBase: BUILD_DIR,
+  //   port: 3333
+  // },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  plugins: [
+    //Built in plugin UglifyJsPlugin
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ]
+}
+
+module.exports = config
